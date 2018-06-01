cd0788b2365827261d44758f809f829835c8c850
Anne van Rossum <anne@crownstone.rocks>
2018-06-01 13:44:14 +0200

    Remove drafts

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -3,8 +3,10 @@ module Jekyll
 		# Check also: https://stackoverflow.com/questions/11410611/get-jekyll-configuration-inside-plugin
 		def generate(site)
 			site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
-			
-			site.config['JB']['analytics']['google']['tracking_id'] = ENV['GOOGLE_ANALYTICS_KEY'] || 'ga-unknown'
+
+			if site.config['JB']['analytics']['google'] 
+				site.config['JB']['analytics']['google']['tracking_id'] = ENV['GOOGLE_ANALYTICS_KEY'] || 'ga-unknown'
+			end
 		end
 
 	end
