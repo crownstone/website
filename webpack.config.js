b086045e589167cacd556e4f3b6e42c9c4c6b53b
Anne van Rossum <anne@crownstone.rocks>
2020-07-22 10:28:24 +0200

    Set webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./webpack-src/entry.js",
-  output: {
-    path: path.resolve(__dirname, "assets/themes/flat/js/"),
-    filename: "bundle.js"
-  },
-  module: {
-  rules: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      loader: "babel-loader", 
-      query: {
-        presets: ["@babel/react", "@babel/env"]
-      }
-    }
-    ]
-  }
+	entry: "./webpack-src/entry.js",
+	mode: "production",
+	output: {
+		path: path.resolve(__dirname, "assets/themes/flat/js/"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules)/,
+				loader: "babel-loader", 
+				query: {
+					presets: ["@babel/react", "@babel/env"]
+				}
+			}
+		]
+	}
 };
